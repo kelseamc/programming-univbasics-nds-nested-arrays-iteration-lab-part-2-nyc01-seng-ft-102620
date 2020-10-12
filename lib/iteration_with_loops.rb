9c943f3648df3f202bc8678c669fa0182b818cea
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-10-12 19:22:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def find_min_in_nested_arrays(src)
   row_index = 0
   low_temps = []
   while row_index < src.length do
-    element_index = 0 
+    element_index = 0
     min_temp = ""
     while element_index < src[row_index].length do
       if src[row_index][element_index] < min_temp
@@ -17,4 +17,3 @@ def find_min_in_nested_arrays(src)
   end
   low_temps
 end
-  
